85596b729fa61aea18f3ed7a2f98ed9e2baeba68
HashQ <ishashq@163.com>
2022-12-21 17:59:54 +0800

    修改心跳handler

--- src/main/java/cn/hashq/netpoststation/handler/HeartHandler.java
+++ src/main/java/cn/hashq/netpoststation/handler/HeartHandler.java
@@ -1,9 +1,15 @@
 package cn.hashq.netpoststation.handler;
 
-import io.netty.channel.ChannelHandler;
-import io.netty.channel.ChannelInboundHandlerAdapter;
+import cn.hashq.netpoststation.dto.ProtoMsg;
+import cn.hashq.netpoststation.session.ServerSession;
+import cn.hashq.netpoststation.util.ThreadUtil;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.handler.timeout.IdleStateEvent;
+import io.netty.handler.timeout.IdleStateHandler;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.stereotype.Component;
+
+import java.util.Objects;
+import java.util.concurrent.TimeUnit;
 
 /**
  * 处理心跳Handler
@@ -12,8 +18,35 @@ import org.springframework.stereotype.Component;
  * @since 1.0
  */
 @Slf4j
-@Component
-@ChannelHandler.Sharable
-public class HeartHandler extends ChannelInboundHandlerAdapter {
+public class HeartHandler extends IdleStateHandler {
 
+    private static final int READ_IDLE_GAP = 150;
+
+    public HeartHandler() {
+        super(READ_IDLE_GAP, 0, 0, TimeUnit.SECONDS);
+    }
+
+    @Override
+    public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+        if (Objects.isNull(msg) || !(msg instanceof ProtoMsg.Message)) {
+            super.channelRead(ctx, msg);
+            return;
+        }
+        ProtoMsg.Message pkg = (ProtoMsg.Message) msg;
+        ProtoMsg.HeadType type = pkg.getType();
+        if (type.equals(ProtoMsg.HeadType.HEART_BEAT)) {
+            ThreadUtil.getIOIntenseTargetThreadPool().submit(() -> {
+                if (ctx.channel().isActive()) {
+                    ctx.writeAndFlush(msg);
+                }
+            });
+        }
+        super.channelRead(ctx, msg);
+    }
+
+    @Override
+    protected void channelIdle(ChannelHandlerContext ctx, IdleStateEvent evt) throws Exception {
+        log.info("{}秒内没读到数据，关闭连接", READ_IDLE_GAP);
+        ServerSession.closeSession(ctx);
+    }
 }
