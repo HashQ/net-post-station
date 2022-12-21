54c24264757a4689ee486c76ce8619f458af2014
HashQ <ishashq@163.com>
2022-12-21 17:59:41 +0800

    修改异常handler

--- src/main/java/cn/hashq/netpoststation/handler/ExceptionHandler.java
+++ src/main/java/cn/hashq/netpoststation/handler/ExceptionHandler.java
@@ -1,10 +1,13 @@
 package cn.hashq.netpoststation.handler;
 
+import cn.hashq.netpoststation.exception.InvalidFrameException;
 import io.netty.channel.ChannelHandler;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.ChannelInboundHandlerAdapter;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
+
+import java.io.IOException;
 
 /**
  * 流水线中异常处理
@@ -19,7 +22,16 @@ public class ExceptionHandler extends ChannelInboundHandlerAdapter {
 
     @Override
     public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
-        super.exceptionCaught(ctx, cause);
+        if (cause instanceof InvalidFrameException) {
+            log.error("报文异常,异常信息:{}", cause.getMessage());
+        }
+        if (cause instanceof IOException) {
+            log.info("客户端关闭链接");
+            // TODO:关闭链接，清除缓存
+        } else {
+            log.error("未知异常,异常信息:{}", cause.getMessage());
+            cause.printStackTrace();
+        }
     }
 
     @Override
@@ -29,6 +41,6 @@ public class ExceptionHandler extends ChannelInboundHandlerAdapter {
 
     @Override
     public void channelActive(ChannelHandlerContext ctx) throws Exception {
-        
+
     }
 }
