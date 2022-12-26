c8deee45367a5c42caa4a754d445f24bb5d73fe7
HashQ <ishashq@163.com>
2022-12-26 10:16:33 +0800

    端口映射管理

---
+++ src/main/java/cn/hashq/netpoststation/service/impl/PortMapManagerServiceImpl.java
@@ -0,0 +1,16 @@
+package cn.hashq.netpoststation.service.impl;
+
+import cn.hashq.netpoststation.dto.AddPortMapDTO;
+import cn.hashq.netpoststation.service.PortMapManagerService;
+import cn.hashq.netpoststation.vo.ServerResponse;
+
+public class PortMapManagerServiceImpl implements PortMapManagerService {
+
+    @Override
+    public ServerResponse addPortMap(AddPortMapDTO portMapDTO) {
+
+        return null;
+    }
+
+
+}
